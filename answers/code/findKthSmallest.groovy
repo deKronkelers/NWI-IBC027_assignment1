2d1ec7fae6813aa6c14e5206d1758af4832c71f0
Hendrikto <hendrik.to@gmail.com>
2016-09-11 18:22:49 +0200

    Add an answer stub for 1.6.

---
+++ answers/code/findKthSmallest.groovy
@@ -0,0 +1,12 @@
+/**
+ * Find the kth-smallest element in the union of two sorted arrays in
+ * O(log m + log n) where m and n are the sizes of the two arrays.
+ *
+ * @param m the first sorted array
+ * @param n the second sorted array
+ * @param k
+ * @return the kth-smallest element in the union of m and n
+ */
+int findKthSmallest(int[] m, int[] n, int k) {
+
+}
