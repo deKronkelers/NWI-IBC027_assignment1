8d583e9b1c733b72123f48f378e8d699489e4eee
Hendrikto <hendrik.to@gmail.com>
2016-09-13 17:58:12 +0200

    Add explanation of why 1.6 could not be solved.

--- answers/code/findKthSmallest.groovy
+++ answers/code/findKthSmallest.groovy
@@ -9,5 +9,8 @@
  * @author Hendrik Werner
  */
 int findKthSmallest(int[] m, int[] n, int k) {
-
+	// if ?? return m[0]?? or n[0]??
+	// compare some elements of m and n
+	// throw away a fraction of both m and n
+	// recursively call this with the reduced m and n
 }
